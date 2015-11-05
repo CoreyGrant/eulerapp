6ed2750cba43a58685c25cce306d76061450a83a
CoreyGrant <corey.grant.durham@hotmail.co.uk>
2015-11-05 22:02:22 +0000

    sasses some things

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-contrib-sass')
 	
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -19,9 +20,28 @@ module.exports = function(grunt){
 			}
 		},
 		watch:{
-			files: 'scripts/*',
-			tasks: ['default']
+			css: {
+				files: 'sass/*',
+				tasks: ['sass']
+			},
+			js: {
+				files: ['scripts/*'],
+				tasks: ['js']
+			}
+		},
+		sass:{
+			dist:{
+				files:[{
+					expand: true,
+					cwd: 'sass',
+					src: ['*.scss'],
+					dest: 'public/stylesheets',
+					ext: '.css'
+				}]
+			}
 		}
 	});
-	grunt.registerTask('default', ['browserify']);
+	grunt.registerTask('js', ['browserify']);
+	grunt.registerTask('css', ['sass']);
+	grunt.registerTask('default', ['browserify', 'sass']);
 }
