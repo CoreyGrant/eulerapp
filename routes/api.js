1a546a77f4c688fc6986228fe4cef52d3b44258a
CoreyGrant <corey.grant.durham@hotmail.co.uk>
2015-11-30 21:58:03 +0000

    Space issue fixed

--- routes/api.js
+++ routes/api.js
@@ -30,12 +30,13 @@ router.get('/problem', function(req, res, next) {
 				var $ = cheerio.load(html);
 				var content = $('#content')
 				fileData.title = content.find('h2').text();
+				// ToDo: Figure out how to replace with reduce
 				var questionDesc = "";
 				content
 					.find('.problem_content')
 					.find('p')
-					.each(function(){questionDesc += $(this).text()});
-				fileData.questionDesc = questionDesc;
+					.each(function(){questionDesc += " " + $(this).text()});
+				fileData.questionDesc = questionDesc.trim();
 				fs.writeFile(filePath, JSON.stringify(file), function(err){});
 				res.send(fileData);
 			});
